e7c1b7caa79f911a24ae718cd63033d096395899
Liangdong Xu <liangdox@andrew.cmu.edu>
2022-03-27 23:52:34 -0400

    Update auto.bat

---
+++ auto.bat
@@ -0,0 +1,20 @@
+E:
+cd E:\VirtualBox\vagrant\
+mkdir hw1-4
+cd hw1-4
+
+powershell curl -o "xenial-server-cloudimg-amd64-vagrant-2.box" "https://app.vagrantup.com/ubuntu/boxes/xenial64/versions/20211001.0.0/providers/virtualbox.box"
+
+vagrant box add .\xenial-server-cloudimg-amd64-vagrant-2.box --name assignment1
+mkdir vagrant
+cd vagrant
+
+vagrant init assignment1
+
+powershell curl -o "Vagrantfile" "https://raw.githubusercontent.com/Dedsec-Xu/devops/main/Vagrantfile"
+powershell curl -o "jenkins.sh" "https://raw.githubusercontent.com/Dedsec-Xu/devops/main/jenkins.sh"
+vagrant up
+
+
+
+
