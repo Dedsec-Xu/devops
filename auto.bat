c59ab983ac5a2c36493467a7528bfb3171636230
Liangdong Xu <liangdox@andrew.cmu.edu>
2022-03-28 01:27:18 -0400

    1

--- auto.bat
+++ auto.bat
@@ -3,9 +3,9 @@ cd E:\VirtualBox\vagrant\
 mkdir hw1-4
 cd hw1-4
 
-powershell curl -o "xenial-server-cloudimg-amd64-vagrant-2.box" "https://app.vagrantup.com/ubuntu/boxes/xenial64/versions/20211001.0.0/providers/virtualbox.box"
+@REM powershell curl -o "xenial-server-cloudimg-amd64-vagrant-2.box" "https://app.vagrantup.com/ubuntu/boxes/xenial64/versions/20211001.0.0/providers/virtualbox.box"
 
-vagrant box add .\xenial-server-cloudimg-amd64-vagrant-2.box --name assignment1
+vagrant box add E:\VirtualBox\.vagrant.d\xenial-server-cloudimg-amd64-vagrant-2.box --name assignment1
 mkdir vagrant
 cd vagrant
 
