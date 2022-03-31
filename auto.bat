7fd71240eb318dfa4fce81fa03c2c41ac0758ce6
Liangdong Xu <liangdox@andrew.cmu.edu>
2022-03-31 00:34:32 -0400

    Update auto.bat

--- auto.bat
+++ auto.bat
@@ -1,18 +1,16 @@
 E:
 cd E:\VirtualBox\vagrant\
-mkdir hw1-5
-cd hw1-5
+mkdir hw1-13
+cd hw1-13
 
-@REM powershell curl -o "xenial-server-cloudimg-amd64-vagrant-2.box" "https://app.vagrantup.com/ubuntu/boxes/xenial64/versions/20211001.0.0/providers/virtualbox.box"
-
-vagrant box add E:\VirtualBox\.vagrant.d\xenial-server-cloudimg-amd64-vagrant-2.box --name assignment1
 mkdir vagrant
 cd vagrant
 
 vagrant init assignment1
 
-powershell curl -o "Vagrantfile" "https://raw.githubusercontent.com/Dedsec-Xu/devops/main/Vagrantfile"
-powershell curl -o "jenkins.sh" "https://raw.githubusercontent.com/Dedsec-Xu/devops/main/jenkins.sh"
+powershell curl -o "Vagrantfile" "https://raw.githubusercontent.com/Dedsec-Xu/devops/main/current/Vagrantfile"
+powershell curl -o "mem.sh" "https://raw.githubusercontent.com/Dedsec-Xu/devops/main/current/mem.sh"
+powershell curl -o "docker-compose.yml" "https://raw.githubusercontent.com/Dedsec-Xu/devops/main/current/docker-compose.yml"
 vagrant up
 
 
