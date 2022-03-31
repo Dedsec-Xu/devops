249292b86226ccff94f3d5ac2d1e193a740cc88e
Liangdong Xu <liangdox@andrew.cmu.edu>
2022-03-31 15:31:12 -0400

    1
    1

--- current/mem.sh
+++ current/mem.sh
@@ -11,3 +11,20 @@ sudo apt-get install wget unzip -y
 
 sudo apt-get install openjdk-11-jdk -y
 sudo apt-get install openjdk-11-jre -y
+
+
+sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" >> /etc/apt/sources.list.d/pgdg.list'
+wget -q https://www.postgresql.org/media/keys/ACCC4CF8.asc -O - | sudo apt-key add -
+sudo apt-get -y install postgresql postgresql-contrib
+sudo systemctl start postgresql
+sudo systemctl enable postgresql
+
+# sudo passwd postgres
+# su - postgres
+# createuser sonar
+# psql
+# ALTER USER sonar WITH ENCRYPTED password 'sonar';
+# CREATE DATABASE sonarqube OWNER sonar;
+# grant all privileges on DATABASE sonarqube to sonar;
+# \q
+# exit
