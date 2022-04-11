d587e73b34e30f43a2741ea74fca77625d2ff912
Liangdong Xu <liangdox@andrew.cmu.edu>
2022-04-11 18:23:05 -0400

    1

--- hw2/ansible/provision.sh
+++ hw2/ansible/provision.sh
@@ -4,3 +4,6 @@ sudo apt-get install wget unzip -y
 
 sudo apt-get install openjdk-11-jdk -y
 sudo apt-get install openjdk-11-jre -y
+
+sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config; sudo systemctl restart sshd;
+
--- hw2/auto.bat
+++ hw2/auto.bat
@@ -1,17 +1,29 @@
 E:
 cd E:\VirtualBox\vagrant\
-mkdir hw1-demo
-cd hw1-demo
+mkdir hw22
+cd hw22
+
+
+mkdir ansible
+cd ansible
+vagrant init
+
+powershell curl -o "Vagrantfile" "https://raw.githubusercontent.com/Dedsec-Xu/devops/main/hw2/ansible/Vagrantfile"
+powershell curl -o "provision.sh" "https://github.com/Dedsec-Xu/devops/blob/main/hw2/ansible/provision.sh"
+
 
 mkdir vagrant
 cd vagrant
 
 vagrant init
 
-powershell curl -o "Vagrantfile" "https://raw.githubusercontent.com/Dedsec-Xu/devops/main/current/Vagrantfile"
-powershell curl -o "mem.sh" "https://raw.githubusercontent.com/Dedsec-Xu/devops/main/current/mem.sh"
-powershell curl -o "docker-compose.yml" "https://raw.githubusercontent.com/Dedsec-Xu/devops/main/current/docker-compose.yml"
-powershell curl -o "sonar-setup.sh" "https://raw.githubusercontent.com/Dedsec-Xu/devops/main/current/sonar-setup.sh"
+powershell curl -o "Vagrantfile" "https://raw.githubusercontent.com/Dedsec-Xu/devops/main/hw2/vagrant/Vagrantfile"
+powershell curl -o "provision.sh" "https://raw.githubusercontent.com/Dedsec-Xu/devops/main/hw2/vagrant/provision.sh"
+powershell curl -o "docker-compose.yml" "https://raw.githubusercontent.com/Dedsec-Xu/devops/main/hw2/vagrant/docker-compose.yml"
+powershell curl -o "ansible.cfg" "https://raw.githubusercontent.com/Dedsec-Xu/devops/main/hw2/vagrant/ansible.cfg"
+powershell curl -o "hosts" "https://raw.githubusercontent.com/ansible/ansible/devel/examples/hosts"
+powershell curl -o "petclinic.jar" "https://raw.githubusercontent.com/ansible/ansible/devel/examples/petclinic.jar"
+
 vagrant up
 
 
