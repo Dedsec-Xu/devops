7a799aacd2b668b16c5e44e3deb4c441fa6511ac
Liangdong Xu <liangdox@andrew.cmu.edu>
2022-04-11 18:19:59 -0400

    Update provision.sh

--- hw2/vagrant/provision.sh
+++ hw2/vagrant/provision.sh
@@ -20,12 +20,9 @@ vagrant init
 powershell curl -o "Vagrantfile" "https://raw.githubusercontent.com/Dedsec-Xu/devops/main/hw2/vagrant/Vagrantfile"
 powershell curl -o "provision.sh" "https://raw.githubusercontent.com/Dedsec-Xu/devops/main/hw2/vagrant/provision.sh"
 powershell curl -o "docker-compose.yml" "https://raw.githubusercontent.com/Dedsec-Xu/devops/main/hw2/vagrant/docker-compose.yml"
-
 powershell curl -o "ansible.cfg" "https://raw.githubusercontent.com/Dedsec-Xu/devops/main/hw2/vagrant/ansible.cfg"
 powershell curl -o "hosts" "https://raw.githubusercontent.com/ansible/ansible/devel/examples/hosts"
 powershell curl -o "petclinic.jar" "https://raw.githubusercontent.com/ansible/ansible/devel/examples/petclinic.jar"
-
-
 
 vagrant up
 
