f7c7bc5c91f3c8590d56616485f7e36fe221d107
Liangdong Xu <liangdox@andrew.cmu.edu>
2022-04-11 18:19:49 -0400

    hw2

---
+++ hw2/vagrant/provision.sh
@@ -0,0 +1,34 @@
+E:
+cd E:\VirtualBox\vagrant\
+mkdir hw22
+cd hw22
+
+
+mkdir ansible
+cd ansible
+vagrant init
+
+powershell curl -o "Vagrantfile" "https://raw.githubusercontent.com/Dedsec-Xu/devops/main/hw2/ansible/Vagrantfile"
+powershell curl -o "provision.sh" "https://github.com/Dedsec-Xu/devops/blob/main/hw2/ansible/provision.sh"
+
+
+mkdir vagrant
+cd vagrant
+
+vagrant init
+
+powershell curl -o "Vagrantfile" "https://raw.githubusercontent.com/Dedsec-Xu/devops/main/hw2/vagrant/Vagrantfile"
+powershell curl -o "provision.sh" "https://raw.githubusercontent.com/Dedsec-Xu/devops/main/hw2/vagrant/provision.sh"
+powershell curl -o "docker-compose.yml" "https://raw.githubusercontent.com/Dedsec-Xu/devops/main/hw2/vagrant/docker-compose.yml"
+
+powershell curl -o "ansible.cfg" "https://raw.githubusercontent.com/Dedsec-Xu/devops/main/hw2/vagrant/ansible.cfg"
+powershell curl -o "hosts" "https://raw.githubusercontent.com/ansible/ansible/devel/examples/hosts"
+powershell curl -o "petclinic.jar" "https://raw.githubusercontent.com/ansible/ansible/devel/examples/petclinic.jar"
+
+
+
+vagrant up
+
+
+
+
