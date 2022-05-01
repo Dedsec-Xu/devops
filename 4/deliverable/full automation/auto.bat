f771819ce4310ee6d2aeb7a1a2ed4880ff8be9c3
Liangdong Xu <liangdox@andrew.cmu.edu>
2022-05-01 15:19:42 -0400

    Update auto.bat

--- 4/deliverable/full automation/auto.bat
+++ 4/deliverable/full automation/auto.bat
@@ -11,7 +11,8 @@ vagrant init
 powershell curl -o "1.conf" "https://raw.githubusercontent.com/Dedsec-Xu/devops/main/4/scripts/ansible/1.conf"
 powershell curl -o "Vagrantfile" "https://raw.githubusercontent.com/Dedsec-Xu/devops/main/4/scripts/ansible/Vagrantfile"
 powershell curl -o "logstash.log" "https://raw.githubusercontent.com/Dedsec-Xu/devops/main/4/scripts/ansible/logstash.log"
-powershell curl -o "provision.sh" "https://raw.githubusercontent.com/Dedsec-Xu/devops/main/4/scripts/ansible/provision.sh"
+powershell curl -o "petclinic.log" "https://raw.githubusercontent.com/Dedsec-Xu/devops/main/4/scripts/ansible/petclinic.log"
+powershell curl -o "petcliniclog.csv" "https://raw.githubusercontent.com/Dedsec-Xu/devops/main/4/scripts/ansible/petcliniclog.csv"
 powershell curl -o "provision.sh" "https://raw.githubusercontent.com/Dedsec-Xu/devops/main/4/scripts/ansible/provision.sh"
 
 vagrant up
@@ -24,11 +25,12 @@ cd vagrant
 
 vagrant init
 
-powershell curl -o "Vagrantfile" "https://raw.githubusercontent.com/Dedsec-Xu/devops/main/hw2/vagrant/Vagrantfile"
-powershell curl -o "provision.sh" "https://raw.githubusercontent.com/Dedsec-Xu/devops/main/hw2/vagrant/provision.sh"
-powershell curl -o "docker-compose.yml" "https://raw.githubusercontent.com/Dedsec-Xu/devops/main/hw2/vagrant/docker-compose.yml"
-powershell curl -o "ansible.cfg" "https://raw.githubusercontent.com/Dedsec-Xu/devops/main/hw2/vagrant/ansible.cfg"
-powershell curl -o "hosts" "https://raw.githubusercontent.com/Dedsec-Xu/devops/main/hw2/vagrant/hosts"
-powershell curl -o "petclinic.jar" "https://raw.githubusercontent.com/Dedsec-Xu/devops/main/hw2/vagrant/petclinic.jar"
+powershell curl -o "Vagrantfile" "https://raw.githubusercontent.com/Dedsec-Xu/devops/main/4/scripts/vagrant/Vagrantfile"
+powershell curl -o "provision.sh" "https://raw.githubusercontent.com/Dedsec-Xu/devops/main/4/scripts/vagrant/provision.sh"
+powershell curl -o "docker-compose.yml" "https://raw.githubusercontent.com/Dedsec-Xu/devops/main/4/scripts/vagrant/docker-compose.yml"
+powershell curl -o "ansible.cfg" "https://raw.githubusercontent.com/Dedsec-Xu/devops/main/4/scripts/vagrant/ansible.cfg"
+powershell curl -o "hosts" "https://raw.githubusercontent.com/Dedsec-Xu/devops/main/4/scripts/vagrant/hosts"
+powershell curl -o "petclinic.jar" "https://raw.githubusercontent.com/Dedsec-Xu/devops/main/4/scripts/vagrant/petclinic.jar"
+powershell curl -o "play.yml" "https://raw.githubusercontent.com/Dedsec-Xu/devops/main/4/scripts/vagrant/play.yml"
 
 vagrant up
