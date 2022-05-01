4a946f9eb4f5537ef2aedf534e0df1d7e0d7abe1
Liangdong Xu <liangdox@andrew.cmu.edu>
2022-05-01 15:16:45 -0400

    1

--- 4/deliverable/full automation/auto.bat
+++ 4/deliverable/full automation/auto.bat
@@ -7,3 +7,28 @@ cd hw4-final
 mkdir ansible
 cd ansible
 vagrant init
+
+powershell curl -o "1.conf" "https://raw.githubusercontent.com/Dedsec-Xu/devops/main/4/scripts/ansible/1.conf"
+powershell curl -o "Vagrantfile" "https://raw.githubusercontent.com/Dedsec-Xu/devops/main/4/scripts/ansible/Vagrantfile"
+powershell curl -o "logstash.log" "https://raw.githubusercontent.com/Dedsec-Xu/devops/main/4/scripts/ansible/logstash.log"
+powershell curl -o "provision.sh" "https://raw.githubusercontent.com/Dedsec-Xu/devops/main/4/scripts/ansible/provision.sh"
+powershell curl -o "provision.sh" "https://raw.githubusercontent.com/Dedsec-Xu/devops/main/4/scripts/ansible/provision.sh"
+
+vagrant up
+
+
+
+cd ..
+mkdir vagrant
+cd vagrant
+
+vagrant init
+
+powershell curl -o "Vagrantfile" "https://raw.githubusercontent.com/Dedsec-Xu/devops/main/hw2/vagrant/Vagrantfile"
+powershell curl -o "provision.sh" "https://raw.githubusercontent.com/Dedsec-Xu/devops/main/hw2/vagrant/provision.sh"
+powershell curl -o "docker-compose.yml" "https://raw.githubusercontent.com/Dedsec-Xu/devops/main/hw2/vagrant/docker-compose.yml"
+powershell curl -o "ansible.cfg" "https://raw.githubusercontent.com/Dedsec-Xu/devops/main/hw2/vagrant/ansible.cfg"
+powershell curl -o "hosts" "https://raw.githubusercontent.com/Dedsec-Xu/devops/main/hw2/vagrant/hosts"
+powershell curl -o "petclinic.jar" "https://raw.githubusercontent.com/Dedsec-Xu/devops/main/hw2/vagrant/petclinic.jar"
+
+vagrant up
